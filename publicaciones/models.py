d66a78fcc214bc01cff1b9db8ea3b50c8c46f8e7
Sergio Roales Garcia <roales.sergio04@gmail.com>
2025-12-18 12:32:02 +0000

    contraseñe

--- publicaciones/models.py
+++ publicaciones/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Usuario(models.Model):
     nombre = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
-
+    password = models.CharField(max_length=128, blank=True)
     def __str__(self):
         return self.nombre
 
