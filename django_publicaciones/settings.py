ba0ad2a281238b34aa7cf6bed620ad27afc91b61
Sergio Roales Garcia <roales.sergio04@gmail.com>
2025-12-18 11:25:14 +0000

    Desarrollo

--- django_publicaciones/settings.py
+++ django_publicaciones/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-wu1)t7(o$$kv1i@c=69h&uj$+v+s=-r3zl=4)k0wu*nyf2!db$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -122,3 +122,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = BASE_DIR / "static_collected"
